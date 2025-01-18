a302bddfbe95457ae28d6ca16a042251e28bcda2
Denilson <denilsoncdc10@gmail.com>
2025-01-18 02:32:35 -0300

    Initial commit after resetting Git

---
+++ TaskManagementSystem.Domain/Entities/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementSystem.Domain.Entities
+{
+    public class Task
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string? Description { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Task(string title, string? description)
+        {
+            Title = title;
+            Description = description;
+            IsCompleted = false;
+            CreatedAt = DateTime.Now;
+            UpdatedAt = DateTime.Now; 
+        }
+
+        public void Update(string title, string? description, bool isCompleted)
+        {
+            Title = title;
+            Description = description;
+            IsCompleted = isCompleted;
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ TaskManagementSystem.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace TaskManagementSystem.Shared;
+
+public class Class1
+{
+
+}
